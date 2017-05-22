38f0c9f2e120023a4547bfb570255ceaafc7d3ae
Marc-Velay <marc.velay@gmail.com>
2017-05-22 15:33:09 +0000

    added templating

--- app/views.py
+++ app/views.py
@@ -1,6 +1,23 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
+def hello_world():
+    return 'Welcome Flask on Predix :)' 
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+    return render_template('index/index.html',
+                           title='Home',
+                           posts=posts,
+                           user=user)
--- run.py
+++ run.py
@@ -1,13 +1,10 @@
 from flask import Flask
 import os
-
-app = Flask(__name__)
+from app import app
+#app = Flask(__name__)
 
 port = int(os.getenv("PORT", 3000))
 
-@app.route('/')
-def hello_world():
-    return 'Welcome Flask on Predix :)' 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
