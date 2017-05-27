1de6bf9cb37c476b977a4afbf888bf168b45418f
Marc-Velay <marc.velay@gmail.com>
2017-05-27 17:04:06 +0000

    successful TS querries in /test

--- app/views.py
+++ app/views.py
@@ -7,12 +7,10 @@ import base64
 import os
 import pandas as pd
 import base64
-import json
+import json, codecs
+from datetime import datetime
 from app import app
 from .forms import LoginForm
-#from bokeh.io import output_notebook
-#from bokeh.charts import TimeSeries, output_file, show 
-#output_notebook()
 
 
 ## SET this only for local testing, if VCAPS is set env they will be overwritten by VCAPS.
@@ -23,15 +21,18 @@ port = int(os.getenv("PORT", 3000))
 REDIRECT_URI = "http://localhost:3000/callback" #"http://localhost:"+str(port)+"/callback"
 
 uaaUrl = "https://iiotquest-uaa-service.predix-uaa.run.aws-usw02-pr.ice.predix.io"
-#tsUrl = "https://time-series-store-predix.run.aws-usw02-pr.ice.predix.io/v1/datapoints"
+tsDataUrl = "https://time-series-store-predix.run.aws-usw02-pr.ice.predix.io/v1/datapoints"
 tsUrl = "https://time-series-store-predix.run.aws-usw02-pr.ice.predix.io/v1/tags"
-payload_last = "{\"tags\": [{\"name\": \"Compressor-2015:CompressionRatio\"}]}"
-payload_first = "{\"tags\": [{\"name\": \"Compressor-2015:CompressionRatio\"}]}"
+requestTags = "{\"tags\": [{\"name\": \"Compressor-2015:CompressionRatio\"}]}"
+#requestData = "{  \"start\": \"50y-ago\",  \"tags\": [    {      \"name\": \"Flight_13.Air_Density_ambient_kg_m\",      \"order\": \"asc\",      \"limit\": 1    }  ]}"
+requestData = "{ \"tags\": [    {      \"name\": \"Flight_13.Air_Density_ambient_kg_m\",     }  ]}"
+requestData_last = "{  \"start\": \"50y-ago\",  \"tags\": [    {      \"name\": \"Flight_13.Air_Density_ambient_kg_m\",      \"order\": \"desc\",      \"limit\": 1    }  ]}"
+requestData_first = "{  \"start\": \"50y-ago\",  \"tags\": [    {      \"name\": \"Flight_13.Air_Density_ambient_kg_m\",      \"order\": \"asc\",      \"limit\": 1    }  ]}"
 zoneId = "6138f224-a4d2-4329-8a10-5ffdc3d4ca9f"
 tokents = base64.b64encode('timeseries_client_readonly:IM_SO_SECRET')
 
 ## Setting up Oauth2 , this values should be read from vcaps .
-APP_URL= None
+APP_URL= "http://localhost:3000/"
 # Get UAA credentials from VCAPS
 if 'VCAP_SERVICES' in os.environ:
     services = json.loads(os.getenv('VCAP_SERVICES'))
@@ -48,40 +49,37 @@ else :
     APP_URL = "http://localhost:"+str(port)
     REDIRECT_URI = APP_URL+"/callback"
 
+#Cloud env defined variable for services, see manifest file
+#used for identification on services
 if(os.getenv('client_id')):
     CLIENT_ID = os.getenv('client_id')
 
+#Cloud env defined variable for services, see manifest file
+#Used for authentification on services
 if(os.getenv('base64encodedClientDetails')):
     BASE64ENCODING = os.getenv('base64encodedClientDetails')
 
 
 @app.route('/')
 def home():
-    #print 'Calling root resource'
-    #text = '<br> <a href="%s">Authenticate with Predix UAA </a>'
-    #return 'Hello from Python microservice template!'+text % getUAAAuthorizationUrl()
-
-    key = session.get('key', 'not set')
-    if 'access_token' in session:
+    key = session.get('key', 'not set') #UAA session token
+    if 'access_token' in session: # check if user has identified himself
         # TODO: call to Check_token to validate this token
-        #return 'This is a secure page,gated by UAA'
         return redirect(APP_URL+"/index", code=302)
-    else :
-        #text = '<br> <a href="%s">Authenticate with Predix UAA </a>'
-        #return 'Token not found, You are not logged in to UAA '+text % getUAAAuthorizationUrl()
+    else : #if not IDed, redirect to identification url
         return redirect(getUAAAuthorizationUrl(), code=302)
 
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
+    form = LoginForm() #fetch form from forms.py, which include objects and requirements for fields
+    if form.validate_on_submit():   #What to do when submit button is clicked
         flash('Login requested for OpenID="%s", remember_me=%s' %
               (form.openid.data, str(form.remember_me.data)))
         return redirect('/index')
-    return render_template('index/login.html', 
-                           title='Sign In',
+    return render_template('index/login.html', #get the login.html template to fill the base.html page
+                           title='Sign In',     #Passing objects to web pages
                            form=form)
 
 @app.route('/index')
@@ -144,24 +142,7 @@ def index():
         }
     ]
     
-    firstPoint = doQuery(payload_first, tsUrl, uaaUrl, tokents, zoneId)
-    '''startDate =  pd.Timestamp(firstPoint['timestamp'][0])
-    startDate = int(startDate.strftime("%s")) * 1000
-    startDateOrigin = startDate
-    
-    lastPoint = doQuery(payload_last, tsUrl, uaaUrl, tokents, zoneId)
-    endDate =  pd.Timestamp(lastPoint['timestamp'][0])
-    endDate = int(endDate.strftime("%s")) * 1000
-    pdArray = []
-
-    while (startDate < endDate ):
-        payload = { 'cache_time': 0, 'tags': [{'name': 'Flight_128.Air_Density_ambient_kg_m', 'order': 'asc'}], 'start': startDate, 'end': startDate + 10000000}
-        startDate = startDate + 100000000
-        series = doQuery(json.dumps(payload), tsUrl, uaaUrl, tokents, zoneId)
-        pdArray.append(series)
-
-    fullseries = pd.concat(pdArray)
-    '''
+    firstPoint = doQueryTags(requestTags, tsUrl, uaaUrl, tokents, zoneId)
 
     return render_template('index/index.html',
                            title='Home',
@@ -202,6 +183,37 @@ def UAAcallback():
     return redirect(APP_URL+"/", code=302)
    
 
+@app.route('/test')
+def test():
+    
+    firstPoint = doQuery(requestData_first, tsDataUrl, uaaUrl, tokents, zoneId)
+    startDate =  pd.Timestamp(firstPoint['timestamp'][0])
+    startDate = int(startDate.strftime("%s")) * 1000
+    startDateOrigin = startDate
+    
+    lastPoint = doQuery(requestData_last, tsDataUrl, uaaUrl, tokents, zoneId)
+    endDate =  pd.Timestamp(lastPoint['timestamp'][0])
+    endDate = int(endDate.strftime("%s")) * 1000
+    pdArray = []
+
+    while (startDate < endDate ):
+        payload = { 'cache_time': 0, 'tags': [{'name': 'Flight_13.Air_Density_ambient_kg_m', 'order': 'asc'}], 'start': startDate, 'end': startDate + 10000000}
+        startDate = startDate + 100000000
+        series = doQuery(json.dumps(payload, codecs.getwriter('utf-8'), ensure_ascii=False), tsDataUrl, uaaUrl, tokents, zoneId)
+        pdArray.append(series)
+
+    fullseries = pd.concat(pdArray)
+    data = dict(vals = fullseries['values'], Date=fullseries['timestamp'])
+    #print(data)
+    dataJson = json.dumps([{'Date': time, 'val': value} for time, value in zip(data['Date'], data['vals'])], default=json_serial)
+    data = json.loads(dataJson)
+    #print(dataJson)
+
+    return render_template('index/test.html',
+                           title='Testing in progress',
+                           fp=data)
+
+
 # method to consttruct Oauth authorization request
 def getUAAAuthorizationUrl():
     state = 'secure'
@@ -226,16 +238,28 @@ def get_token(code):
     token_json = response.json()
     return token_json["access_token"]
 
+#Adds authentification to the header
 def base_headers():
     return {"Authorization": "Basic "+BASE64ENCODING }
 
+#Was login successful? if yes secure
 def is_valid_state(state):
     if(state == 'secure' ) :
         return True
     else :
         return False
 
-def doQuery(payload, tsUrl, uaaUrl, tokents, zoneId):
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, datetime):
+        serial = obj.isoformat()
+        return serial
+    raise TypeError ("Type not serializable")
+
+
+#prepare querries for Time Series
+def doQueryTags(payload, tsUrl, uaaUrl, tokents, zoneId):
     
     headers = {
         'authorization': 'Basic ' + tokents,
@@ -249,18 +273,43 @@ def doQuery(payload, tsUrl, uaaUrl, tokents, zoneId):
     
     response = requests.request('POST', uaaUrl+"/oauth/token", data=data, headers=headers)
     tokents = json.loads(response.text)['access_token']
-    #return json.loads(response.text)['access_token']
     headers = {
         'authorization': "Bearer " + tokents,
         'predix-zone-id': "" + zoneId,
-        #'content-type': "application/json",
-        #'cache-control': "no-cache"
+        'content-type': "application/json",
+        'cache-control': "no-cache"
     }
     
     response = requests.request("GET", tsUrl, data=payload, headers=headers)
-    data = json.loads(response.text)#['tags'][0]['results'][0]['values']
-    #column_labels = ['timestamp', 'values', 'quality']
-    #series = pd.DataFrame(data, columns=column_labels)
-    #series['timestamp'] = pd.to_datetime(series['timestamp'], unit='ms')
+    data = json.loads(response.text)
     return data
+  
+#prepare querries for Time Series
+def doQuery(payload, tsDataUrl, uaaUrl, tokents, zoneId):
+    
+    headers = {
+        'authorization': 'Basic ' + tokents,
+        'cache-control': 'no-cache',
+        'content-type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+        'client_id':'timeseries_client_readonly',
+        'grant_type':'client_credentials'
+    }
+    
+    response = requests.request('POST', uaaUrl+"/oauth/token", data=data, headers=headers)
+    tokents = json.loads(response.text)['access_token']
+    headers = {
+        'authorization': "Bearer " + tokents,
+        'predix-zone-id': "" + zoneId,
+        'content-type': "application/json",
+        'cache-control': "no-cache"
+    }
+    
+    response = requests.request("POST", tsDataUrl, data=payload, headers=headers)
+    data = json.loads(response.text)['tags'][0]['results'][0]['values']
+    column_labels = ['timestamp', 'values', 'quality']
+    series = pd.DataFrame(data, columns=column_labels)
+    series['timestamp'] = pd.to_datetime(series['timestamp'], unit='ms')
+    return series
     
