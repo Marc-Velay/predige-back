90c048000535e5c56dac2ea9611a9882073347a9
mathieuHa <mathieu.hanotaux@gmail.com>
2017-05-28 23:08:12 +0000

    ajout des graphiques fonctionnel

--- app/static/index/index.js
+++ app/static/index/index.js
@@ -1,9 +1,9 @@
 //alert("works!");
 
-function selectedTag() {
+/*function selectedTag() {
     var selectBox = document.getElementById("tagBoxFlights");
     var selectedValue = selectBox.options[selectBox.selectedIndex].value;
     var base = window.location.href.split('?')[0];
     var flight = window.location.href.split('?')[1].split('&')[0];
     window.location.href = base+'?'+flight+'&tag='+selectedValue;
-}
+}*/
